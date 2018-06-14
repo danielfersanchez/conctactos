4b3b0e89b01692f07de0887b1a4bdbebfbc503ed
danielfersanchez <38057662+danielfersanchez@users.noreply.github.com>
2018-06-13 19:46:36 -0500

    commit

--- conctactos/conctactos.Android/Services/FileHelper.cs
+++ conctactos/conctactos.Android/Services/FileHelper.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 using Android.App;
 using Android.Content;
-using Android.OS;
+
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-
+using conctactos.Droid.Services;
+using conctactos.services;
+[assembly:Dependency(typeof(FileHelper))]
 namespace conctactos.Droid.Services
 {
-    class FileHelper
+    using System;
+    using conctactos.services;
+    using System.IO;
+    public class FileHelper : services.Ifriendhelper
     {
+        public FileHelper()
+        {
+
+        }
+        public string GetLocalFilePath(string fileName)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return path.Combine(path, fileName);
+        }
+                       
     }
+
 }
--- conctactos/conctactos.iOS/Services/FileHelper.cs
+++ conctactos/conctactos.iOS/Services/FileHelper.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-
+using conctactos.iOS.Services;
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
 
+[assembly:Dependency(typeof(FileHelper))]
 namespace conctactos.iOS.Services
 {
-    class FileHelper
+   public  class FileHelper.services.Ifriendhelper
     {
+        public FileHelper() { }
+     public string GetlocalFilePath(string fileName)
+    {
+        string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string libFolder= NSIndexPath.Combine(docFolder,",,","DataBases");
+        if (!Directory.Exists(libFolder))
+        {
+            Directory.CreateDirectory(libFolder);
+        }
+        return Path.Combine(libFolder, fileName);
+    }
     }
 }
